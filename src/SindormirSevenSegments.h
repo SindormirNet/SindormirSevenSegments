26e9bc80b920004e068aa887b0279ec19db4d026
Jorge Gómez <syvic@sindormir.net>
2020-10-07 19:35:21 +0200

    Arreglado código para que funcione con ESP32

--- src/SindormirSevenSegments.h
+++ src/SindormirSevenSegments.h
@@ -21,8 +21,8 @@ Released under the terms of the GPL v3 License
 class SindormirSevenSegments {
     public:
         SindormirSevenSegments(void);
-        void attach(byte _sA, byte _sB, byte _sC, byte _sD, byte _sE, byte _sF, byte _sG, byte _sDP);
-        void attach(byte _sA, byte _sDP);
+        void attach(uint8_t _sA, uint8_t _sB, uint8_t _sC, uint8_t _sD, uint8_t _sE, uint8_t _sF, uint8_t _sG, uint8_t _sDP);
+        void attach(uint8_t _sA, uint8_t _sDP);
         void commonType(char t);
         void lampTest(void);
         void clear(void);
@@ -30,16 +30,16 @@ class SindormirSevenSegments {
         void clearDot(void);
     void print(char n);
     private:
-        byte _segs[8];
+        uint8_t _segs[8];
 
-        byte _sA;
-        byte _sB;
-        byte _sC;
-        byte _sD;
-        byte _sE;
-        byte _sF;
-        byte _sG;
-        byte _sDP;
+        uint8_t _sA;
+        uint8_t _sB;
+        uint8_t _sC;
+        uint8_t _sD;
+        uint8_t _sE;
+        uint8_t _sF;
+        uint8_t _sG;
+        uint8_t _sDP;
 
 	boolean _sT;
 };
